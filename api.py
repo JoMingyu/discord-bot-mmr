cda72e78db3ecf657e591a3b8dbd2cf84ef46980
JoMingyu <mingyu.planb@gmail.com>
2021-09-03 20:21:49 +0900

    v1

---
+++ api.py
@@ -0,0 +1,96 @@
+from abc import abstractmethod, ABC
+from typing import Optional
+
+import arrow
+from pydantic import validator
+from pydantic.main import BaseModel
+from requests import get
+
+
+def italic(text):
+    return f"*{text}*"
+
+
+def bold(text):
+    return f"**{text}**"
+
+
+def underlined(text):
+    return f"__{text}__"
+
+
+class Data(BaseModel):
+    avg: int  # avg mmr
+    err: int  # ± 오차범위
+    closestRank: str
+    percentile: int  # 하위 n%. 100 - percentile은 상위 %를 의미함
+    # warn: bool  # 정확도 낮은 경우 True
+    timestamp: int  # last calculate timestamp
+
+    @property
+    def timestamp_humanized(self) -> str:
+        humanized = (
+            arrow.get(self.timestamp)
+            .humanize()
+            .replace(" days", "일")
+            .replace("a day", "1일")
+            .replace(" hours", "시간")
+            .replace("a hour", "1시간")
+            .replace(" minutes", "분")
+            .replace("a minute", "1분")
+            .replace(" weeks", "주")
+            .replace("a week", "1주")
+            .replace(" ago", " 전")
+        )
+
+        return f"{humanized} 기준"
+
+    def as_message(self) -> str:
+        return f"""
+{bold(self.avg)} ± {self.err} ({self.closestRank} 상위 {100 - self.percentile}%)
+- {italic(self.timestamp_humanized)}
+        """.strip()
+
+
+class MMR(BaseModel):
+    ranked: Optional[Data]  # 솔로랭크
+    normal: Optional[Data]  # 일반게임
+    ARAM: Optional[Data]  # 칼바람 나락
+
+    def as_message(self):
+        solo_rank = italic("N/A") if self.ranked is None else self.ranked.as_message()
+        normal = italic("N/A") if self.normal is None else self.normal.as_message()
+        aram = italic("N/A") if self.ARAM is None else self.ARAM.as_message()
+
+        return f"""
+솔로랭크
+{solo_rank}
+        
+노말
+{normal}
+        
+무작위 총력전
+{aram}
+        """.strip()
+
+    @validator("*", pre=True)
+    def validate_rank_data_not_available(cls, value: dict):
+        if value["avg"] is None:
+            return None
+
+        return value
+
+
+class MMRAPI(ABC):
+    @abstractmethod
+    def get_mmr_data(self, nickname) -> MMR:
+        pass
+
+
+class WhatIsMyMMRAPI(MMRAPI):
+    def get_mmr_data(self, nickname) -> MMR:
+        # TODO error handling - 닉네임 미발견
+
+        resp = get(f"https://kr.whatismymmr.com/api/v1/summoner?name={nickname}")
+
+        return MMR(**resp.json())
--- main.py
+++ main.py
@@ -1,15 +1,45 @@
+# bot.py
 import os
 
 import discord
+from discord import Embed
+from discord.ext import commands
+from discord.ext.commands import Context
 from dotenv import load_dotenv
 
+from api import WhatIsMyMMRAPI
+
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv("DISCORD_TOKEN")
 
-client = discord.Client()
+mmr_api = WhatIsMyMMRAPI()
 
-@client.event
+bot = commands.Bot(command_prefix="/")
+
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print("Booting up your system")
+    print(f"I am running on <{bot.user.name}>")
+    print(f"With the ID: {bot.user.id}")
 
-client.run(TOKEN)
+
+@bot.command("mmr")
+async def on_message(ctx: Context):
+    if ctx.author == bot.user:
+        return
+
+    message: discord.Message = ctx.message
+
+    nickname = message.content.split("/mmr ", 1)[1]
+
+    result = mmr_api.get_mmr_data(nickname)
+
+    resp = result.as_message()
+
+    channel: discord.TextChannel = ctx.channel
+
+    await channel.send(resp)
+
+
+bot.run(TOKEN)
